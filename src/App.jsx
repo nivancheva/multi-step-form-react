1ff038217a7fc1b0eb99f3ae6dfc79810bdef63f
nivancheva <nedyalka.tsintsarova@gmail.com>
2023-11-14 09:29:27 +0200

    add AddOns component

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import './App.css';
 import Buttons from './components/Buttons';
 import PlanPeriod from './components/PlanPeriod';
 import PlanCard from './components/PlanCard';
-import iconArcade from './images/icon-arcade.svg'
+import iconArcade from './images/icon-arcade.svg';
+import AddOns from './components/AddOns';
 
 function App() {
   const [checked, setChecked] = useState(false);
@@ -14,8 +15,11 @@ function App() {
 
   return (
     <div>
+      <AddOns title='Online Service' description='Access to multiplayer games' price='+$1/mo' selected={true}/>
+      <AddOns title='Online Service' description='Access to multiplayer games' price='+$1/mo' selected={false}/>
+
       <PlanCard image ={iconArcade} title="Arcade" price="$90" discount="2 months free" selected={true} />
-      <PlanCard image ={iconArcade} title="Arcade" price="$90" discount="2 months free" selected={false} />
+      
       <PlanPeriod value={checked} onChange={handleChecked}/>
       <Buttons />
     </div>
