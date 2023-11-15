51caa2759fb2a0774419bd64c80411d9779196b5
nivancheva <nedyalka.tsintsarova@gmail.com>
2023-11-15 14:24:29 +0200

    add validation to the inputs

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -1,35 +1,62 @@
 import './Inputs.css';
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
-export default function Inputs() {
+const schema = yup
+  .object({
+    name: yup.string().required('This field is required'),
+    email: yup.string().email().required('This field is required'),
+    tel: yup.number().typeError("This field is required").required(),
+  })
+  .required()
+
+export default function Inputs({ onSubmit }) {
+    function handleFormSubmit(data) {
+        onSubmit(data);
+      }
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+      } = useForm({
+        resolver: yupResolver(schema),
+    })
+
     return (
         <div className='input-wrapper'>
-            <form className='input-form'>
-                <div className='input-container'>
+            <form className='input-form' onSubmit={handleSubmit(handleFormSubmit)}>
+                <div className={`input-container${errors.email?.message ? " input-error" : ""}`}>
                     <label htmlFor='name'>Name</label>
+                    <p className='errors'>{errors.name?.message}</p>
                     <input 
-                        id='name'
+                        {...register("name")}
                         type='text'
                         placeholder='e.g. Stephen King'
                     />
                 </div>
 
-                <div>
+                <div className={`input-container${errors.email?.message ? " input-error" : ""}`}>
                     <label htmlFor='email'>Email Adress</label>
+                    <p className='errors'>{errors.email?.message}</p>
                     <input 
-                        id='email'
+                        {...register("email")}
                         type='email'
                         placeholder='e.g. stephenking@lorem.com'
                     />
                 </div>
 
-                <div>
+                <div className={`input-container${errors.email?.message ? " input-error" : ""}`}>
                     <label htmlFor='tel'>Phone Number</label>
+                    <p className='errors'>{errors.tel?.message}</p>
                     <input 
-                        id='tel'
+                        {...register("tel")}
                         type='tel'
                         placeholder='e.g. +1 234 567 890'
                     />
                 </div>
+                <button type='submit' className='hidden'></button>
             </form>
         </div>
     )
