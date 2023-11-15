0d5ea7ee77098030763f4e91ebcbc4cfd9410d78
nivancheva <nedyalka.tsintsarova@gmail.com>
2023-11-15 16:00:47 +0200

    finish StepTwo component

--- src/step-components/StepOneInputs.jsx
+++ src/step-components/StepOneInputs.jsx
@@ -1,13 +1,15 @@
 import Inputs from "../components/Inputs";
-import './StepOneInputs.css';
+import StepTitle from "./StepTitle";
 
 export default function StepOneInputs() {
     return (
-        <div className="step-one-wrapper">
+        <div>
             <div className="step-one">
                 <h1>Pesonal Info</h1>
                 <p>Please provide your name, email address, and phone number.</p>
             </div>
+            <StepTitle title='Pesonal Info' definition='Please provide your name, email address, and phone number.'/>
+
             <Inputs />
         </div>
     )
