a30a189b17dd2483e12b5c698b8f2a30bfeb9db6
nivancheva <nedyalka.tsintsarova@gmail.com>
2023-11-15 16:28:57 +0200

    fix styles

--- src/step-components/StepTitle.jsx
+++ src/step-components/StepTitle.jsx
@@ -2,9 +2,9 @@ import './StepTitle.css';
 
 export default function StepTitle({ title, definition }) {
     return (
-        <div className="step-title">
+        <div className="title">
             <h1>{title}</h1>
-            <p className="step-title_definition">{definition}</p>
+            <p className="title_definition">{definition}</p>
         </div>
     )
 }
