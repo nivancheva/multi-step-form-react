c9279975daeb3da1f438539a911de918fe5faf8d
nivancheva <nedyalka.tsintsarova@gmail.com>
2023-11-16 13:33:23 +0200

    steps preview functionality

--- src/components/StepsPreview.jsx
+++ src/components/StepsPreview.jsx
@@ -1,48 +1,43 @@
 import './StepsPreview.css';
 
-export default function StepsPreview() {
+const steps = [
+    {
+        value: 1,
+        title: "Your info"
+    },
+    {
+        value: 2,
+        title: "SELECT PLAN"
+    },
+    {
+        value: 3,
+        title: "ADD-ONS"
+    },
+    {
+        value: 4,
+        title: "SUMMARY"
+    }
+];
+
+export default function StepsPreview({ value, onValueChanged }) {
     return (
         <div className='bg-image'>
-            <div className='step-wrapper'>
-                <div>
-                    <span className='step'>1</span>
-                </div>
-                <div className='steps-info'>
-                    <p className='step-num'>Step 1</p>
-                    <p className='step-title'>YOUR INFO</p>
-                </div>
-            </div>
 
-
-            <div className='step-wrapper'>
-                <div>
-                    <span className='step step-active'>2</span>
-                </div>
-                <div className='steps-info'>
-                    <p className='step-num'>Step 2</p>
-                    <p className='step-title'>SELECT PLAN</p>
-                </div>
-            </div>
-
-            <div className='step-wrapper'>
-                <div>
-                    <span className='step'>3</span>
-                </div>
-                <div className='steps-info'>
-                    <p className='step-num'>Step 3</p>
-                    <p className='step-title'>ADD-ONS</p>
-                </div>
-            </div>
-
-            <div className='step-wrapper'>
-                <div>
-                    <span className='step'>4</span>
-                </div>
-                <div className='steps-info'>
-                    <p className='step-num'>Step 4</p>
-                    <p className='step-title'>SUMMARY</p>
-                </div>
-            </div>
+            {steps.map((step, idx) => {
+                const isSelected = step.value === value;
+                return (
+                    <div key={idx} className='step-wrapper'
+                        onClick={() => { onValueChanged(step.value) }}>
+                        <div>
+                            <span className={`step ${isSelected ? "step-active" : ""}`}>{step.value}</span>
+                        </div>
+                        <div className='steps-info'>
+                            <p className='step-num'>Step {step.value}</p>
+                            <p className='step-title'>{step.title}</p>
+                        </div>
+                    </div>   
+                );
+            })}
         </div>
     )
 }
--- src/components/ThankYouCard.jsx
+++ src/components/ThankYouCard.jsx
@@ -3,10 +3,14 @@ import iconThankYou from '../images/icon-thank-you.svg';
 
 export default function ThankYouCard() {
     return (
-        <div className='thank-you-card'>
-            <img className='icon-thank_you' src={iconThankYou}/>
-            <h1>Thank you!</h1>
-            <p>Thanks for confirming your subscription! We hope you have fun using our platform. If you ever need support, please feel free to email us at support@loremgaming.com.</p>
+        <>
+        <div className='steps-section'>
+            <div className='thank-you-card'>
+                <img className='icon-thank_you' src={iconThankYou}/>
+                <h1>Thank you!</h1>
+                <p>Thanks for confirming your subscription! We hope you have fun using our platform. If you ever need support, please feel free to email us at support@loremgaming.com.</p>
+            </div>
         </div>
+        </>
     )
 }
